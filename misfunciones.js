900ead791bea0a899e7f39770ad37a51741a0c48
lalaggv2 <luangu2020@gmail.com>
2022-10-02 21:05:39 -0400

    Logrando armar las tablas y  mostrar los detalles

--- misfunciones.js
+++ misfunciones.js
@@ -1,18 +1,17 @@
-const urlApi ="https://g02ae6c99368ff0-room.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/";
+const urlApi = "https://g02ae6c99368ff0-room.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/";
 
 function traerDatosHotel() {
     $.ajax({
         url: urlApi + "room/room",
         type: 'GET',
         dataType: "json",
-        success: function(respuesta) {
-            pintarDatosGral(respuesta.items, "room");
+        success: function (respuesta) {
+            pintarDatosGral(respuesta.items, "name", "room");
         },
-        error: function(respuesta, xhr){
+        error: function (respuesta, xhr) {
             alert("Error de peticion");
         }
-    })
-
+    });
 }
 
 function traerDatosCliente() {
@@ -20,10 +19,10 @@ function traerDatosCliente() {
         url: urlApi + "client/client",
         type: 'GET',
         dataType: "json",
-        success: function(respuesta) {
-            pintarDatosGral(respuesta.items);
+        success: function (respuesta) {
+            pintarDatosGral(respuesta.items, "name", "client");
         },
-        error: function(respuesta, xhr){
+        error: function (respuesta, xhr) {
             alert("Error de peticion");
         }
     })
@@ -34,61 +33,74 @@ function traerDatosMensajes() {
         url: urlApi + "message/message",
         type: 'GET',
         dataType: "json",
-        success: function(respuesta) {
-            pintarDatosGral(respuesta.items);
+        success: function (respuesta) {
+            pintarDatosGral(respuesta.items, "messageText", "message");
         },
-        error: function(respuesta, xhr){
+        error: function (respuesta, xhr) {
             alert("Error de peticion");
         }
     });
 
 }
 
-function mostrarDetalles(){
-let id = sessionStorage.getItem('id');
-let tipo = sessionStorage.getItem('tipo');
-$.ajax({
-    url:
-    type: 'GET',
-    dataType: 'json',
-    success: function(respuesta){
+function guardarIdyTipo(id, tipo) {
+    sessionStorage.setItem('id', id);
+    sessionStorage.setItem('tipo', tipo);
+    location.href = 'detalles.html';
+}
 
-    }
-})
+function mostrarDetalles() {
+    let id = sessionStorage.getItem('id');
+    let tipo = sessionStorage.getItem('tipo');
+
+    $.ajax({
+        url: urlApi +tipo+'/'+tipo+'/'+id,
+        type: 'GET',
+        dataType: 'json',
+        success: function (respuesta) {
+            pintarDatosDetalles(respuesta.items);
+        },
+        error: function (respuesta, xhr) {
+            alert("Peticion Erronea");
+        }
+    })
 
 }
 
-function pintarDatosGral(datos, clnMostrar){
+function pintarDatosGral(datos, clnMostrar, tipo) {
+    let tipoTabla = tipo;
     let htmlParaInsertar = "";
     htmlParaInsertar += "<tr>";
-    htmlParaInsertar+="<th>Titulo</th>";
-    htmlParaInsertar +="</tr>";
+    htmlParaInsertar += "<th>Titulo</th>";
+    htmlParaInsertar += "</tr>";
 
     for (let index = 0; index < datos.length; index++) {
-        htmlParaInsertar+="<tr>";
-        htmlParaInsertar+="<td><a href='#' onclick='guardarIdyTipo('"+datos[i].id+", \""+[clnMostrar]="</td>";
-        htmlParaInsertar +="</td>"
-}
-
-function guardarIdyTipo(id,tipo){
-
-}
-
-function pintarDatosDetalles(){
-    let htmlParaInsertar = "";
-    htmlParaInsertar += "<tr>";
-    Object.keys(datos[i]).forEach(a +> htmlParaInsertar+="<th>"+a+"</th>");
-    htmlParaInsertar +="</tr>";
-
-    for (let index = 0; index < datos.length; index++) {
-        Object.values(datos[i]).forEach(a => htmlParaInsertar+="<td>"+a+"</td>");
-        htmlParaInsertar +="</td>"
-        
+        htmlParaInsertar += "<tr>";
+        htmlParaInsertar += "<td><a href='detalles.html' onclick='guardarIdyTipo(" + datos[index].id + ", \"" + tipoTabla + "\");'>" + datos[index][clnMostrar] + "</a></td>";
+        htmlParaInsertar += "</td>"
     }
-
+    //document.getElementById("#tabla").innerHTML = texto;
 
     $("#resultado").empty();
     $("#resultado").append(htmlParaInsertar);
-
-
 }
+
+
+
+function pintarDatosDetalles(datos) {
+    let htmlParaInsertar = "";
+    htmlParaInsertar += "<tr>";
+    Object.keys(datos[0]).forEach(elemento => htmlParaInsertar += "<th>" + elemento + "</th>");
+    htmlParaInsertar += "</tr>";
+
+    for (let index = 0; index < datos.length; index++) {
+        htmlParaInsertar += "<tr>"
+        Object.values(datos[index]).forEach(elemento => htmlParaInsertar += "<td>" + elemento + "</td>");
+        htmlParaInsertar += "</tr>"
+    }
+
+    $("#resultado").empty();
+    $("#resultado").append(htmlParaInsertar);
+}
+
+
